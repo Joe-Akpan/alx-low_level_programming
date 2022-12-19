715f7d9b00363fa5c6d1a3f0b7cdfb3ce547e08e
Joseph Akpan <josephakpan316@gmail.com>
2022-12-19 08:03:49 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * Description: main -  program to print text
  * Return: 0 if run successfully  
  */
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
