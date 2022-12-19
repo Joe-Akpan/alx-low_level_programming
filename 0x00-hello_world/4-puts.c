e847a1ce70b91a349736b84b30f6381ccbddc64e
Joseph Akpan <josephakpan316@gmail.com>
2022-12-19 00:32:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
+/*
+ * Description: Main program to print out string
+ *return (0) if successful
+ */
 int main(void)
 {
-	/*
-	* Description: Main program to print out strings for 
-	* a stdio
-	* return (0) if successful
-	*/
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
-
 }
