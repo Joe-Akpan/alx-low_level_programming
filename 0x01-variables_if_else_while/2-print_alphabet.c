85fa6993bd83d006b1fa6ec312828d83b4df2c06
Joseph Akpan <josephakpan316@gmail.com>
2022-12-26 03:22:50 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - thks programe will print alphabets
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++);
+	putchar(alphabets);
+
+	putchar('\n');
+
+	return (0);
+}	
