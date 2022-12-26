c01bb0212e13554e56dfd93aec0196b2e7deefa2
Joseph Akpan <josephakpan316@gmail.com>
2022-12-26 05:15:47 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the program prints alphabets in upper and lower character
+ * Return: 0 if successfull
+*/
+
+int main(void)
+{
+	char gh;
+	
+	for (gh = 'a'; gh <= 'z'; gh++)
+		putchar(gh);
+
+	for (gh = 'A'; gh <= 'Z'; gh++)
+		putchar(gh);
+
+	putchar('\n');
+
+	return (0);
+}
