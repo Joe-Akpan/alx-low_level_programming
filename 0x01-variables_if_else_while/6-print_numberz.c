f2bbb399ddf22c2344e769aee57791b684257881
Joseph Akpan <josephakpan316@gmail.com>
2022-12-26 11:11:19 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -  programs to print number
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		putchar(d);
+
+	putchar('\n');
+
+	return (0);
+}
