bdc3bb4a4a4c6f02f8363089cb9f9770762b8d8b
Joseph Akpan <josephakpan316@gmail.com>
2022-12-28 20:24:19 -0800

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - program to print alphabet in 10 places
+ * Return: always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int check = 0;
+	char alphabet;
+
+	while (check++ <= 9)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+			_putchar('\n');
+		}
+	}
+}
