1fcf0c913c7da0739b98b62babb4389b9a86e0d8
Joseph Akpan <josephakpan316@gmail.com>
2022-12-30 05:48:06 -0800

    10-

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - programs to add two mumber
+ * num1: firat number to add 
+ * num2: second number to add
+ * Return: result
+*/
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
