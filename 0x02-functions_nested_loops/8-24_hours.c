00fabbc47733be5e30da0ea8bae63ece27c46838
Joseph Akpan <josephakpan316@gmail.com>
2022-12-30 03:58:56 -0800

    8-24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * hack_bauer - program to print time
+ * Jack Bauer printing from 0
+*/
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <=23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
