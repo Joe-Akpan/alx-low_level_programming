4e43068e163904241ea0ebbb6cee80b06aed1a25
Joseph Akpan <josephakpan316@gmail.com>
2022-12-30 02:42:47 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - program to print absolut value of integer
+ * @n: the integer to be computed
+ * Return: Absolute value of integer
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
